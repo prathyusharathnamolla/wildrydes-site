2be53cf88ab0accd5bdec10ca1d4dc30c63a34f8
prathyusharathnamolla <78398719+prathyusharathnamolla@users.noreply.github.com>
2025-07-03 20:55:21 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ca-central-1_wPVMWCnOn', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1q4hfaosk56fiss0kf1emmv93o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
