de90e59b121c26bab70e2de027382a842b5adc89
prathyusharathnamolla <78398719+prathyusharathnamolla@users.noreply.github.com>
2025-07-03 21:22:09 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://437k3sgyv5.execute-api.ca-central-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
